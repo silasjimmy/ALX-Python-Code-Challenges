07ae88707e143be544bcdf4aacc63825b469eac1
Silas Jimmy <jimmysilas17@gmail.com>
2025-09-09 17:43:11 +0300

    refactor: implement the bubble sort algorithm

--- algorithmic-complexity/sorting.py
+++ algorithmic-complexity/sorting.py
@@ -39,10 +39,44 @@ def quick_sort(l):
     return small + duplicate + large
 
 
+def bubble_sort(l):
+    '''
+    Implementation of the bubble sort algorithm
+    '''
+    for i in range(len(l)):
+        for j in range(len(l) - 1 - i):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+
+
+def effective_bubble_sort(l):
+    '''
+    Implementation of the effective bubble sort algorithm
+    '''
+    n = len(l)
+
+    while True:
+        swapped = False
+
+        for i in range(1, n):
+            if l[i - 1] > l[i]:
+                l[i - 1], l[i] = l[i], l[i - 1]
+                swapped = True
+
+        if not swapped:
+            break
+
+
 l = [64, 34, 25, 12, 22, 11, 90]
 m = l.copy()
+o = l.copy()
+p = l.copy()
 
 insertion_sort(l)
+bubble_sort(o)
+effective_bubble_sort(p)
 
 print("Insertion sort:", l)
 print("Quick sort:", quick_sort(m))
+print("Bubble sort:", o)
+print("Effective bubble sort:", p)
