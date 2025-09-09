321b05b26cf105a5b396e7228a3b9aa139cf5f4d
Silas Jimmy <jimmysilas17@gmail.com>
2025-09-09 17:28:20 +0300

    refactor: implement the quick sort algorithm

--- algorithmic-complexity/sorting.py
+++ algorithmic-complexity/sorting.py
@@ -12,8 +12,37 @@ def insertion_sort(l):
         l[j + 1] = key
 
 
+def quick_sort(l):
+    '''
+    Implementation of the quick sort algorithm
+    '''
+    if len(l) <= 1:
+        return l
+
+    pivot = l[-1]  # Take the last element to be the pivot
+
+    small = []
+    large = []
+    duplicate = []
+
+    for el in l:
+        if el > pivot:
+            large.append(el)
+        elif el < pivot:
+            small.append(el)
+        elif el == pivot:
+            duplicate.append(el)
+
+    small = quick_sort(small)
+    large = quick_sort(large)
+
+    return small + duplicate + large
+
+
 l = [64, 34, 25, 12, 22, 11, 90]
+m = l.copy()
 
 insertion_sort(l)
 
-print(l)
+print("Insertion sort:", l)
+print("Quick sort:", quick_sort(m))
